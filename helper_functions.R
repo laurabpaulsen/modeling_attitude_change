ea207e338a0e59a41d32e652a0ee1bcc990bf41c
laurabpaulsen <202005791@post.au.dk>
2024-04-10 13:27:44 +0200

    update weighted bayes

--- helper_functions.R
+++ helper_functions.R
@@ -16,22 +16,22 @@ simple_bayes_beta_binomial <- function(source1, source2, bias, lower_bound=1, up
 }
 
 
-weighted_bayes_beta_binomial <- function(source1, source2, weight1, weight2, lower_bound=1, upper_bound=8){
-  n_rows = length(source1)
-  # calculate alpha for beta distribution
+weighted_bayes_beta_binomial <- function(source1, source2, weight1, lower_bound=1, upper_bound=8){
+  # weight1 is between 0 and 2 and is the weight given to the first source
+  # weight 2 is 2 - weight1 and is the weight given to the second source
+  # weight1 + weight2 = 2
   
-  # NOTE: check course notes, riccardo does something where he scales the weights??
-  ## From course notes  w1 <- (w1 - 0.5)*2 and w2 <- (w2 - 0.5)*2
-  alpha <- weight1 * source1 + weight2 * source2 - 2 * lower_bound
+  n_rows = length(source1)
+  alpha <- weight1 * source1 + (2 - weight1) * source2 - 2 * lower_bound
   
   # calculating beta for the beta distribution
-  beta <- 2 * (upper_bound - lower_bound) - alpha
+  beta <- 2 * (upper_bound - lower_bound)
   
   # sample from a beta distribution
   belief <- rbeta(n_rows, alpha, beta)
   
   # predict the rating
-  second_rating <- lower_bound + rbinom(n_rows, upper_bound - lower_bound, belief)
+  second_rating <- as.integer(lower_bound + rbinom(n_rows, upper_bound - lower_bound, belief))
   
   return(second_rating)
 }
